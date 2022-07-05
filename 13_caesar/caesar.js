6bf27048ece7c1e6143245ea3a91487c7ec80202
Soo <soowjo@gmail.com>
2022-07-05 15:12:29 +0900

    Finalize the exercises

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,24 @@
-const caesar = function() {
-
+const caesar = function(str, num) {
+    let upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let lower = 'abcdefghijklmnopqrstuvwxyz';
+    let newStr = ''
+    while (num<0) {
+        num += 26;
+    }
+    for (let i in str) {
+        if (str[i].match(/[A-Z]/)) {
+            let j = upper.indexOf(str[i]);
+            newStr += upper[(j+num)%26];
+        }
+        else if (str[i].match(/[a-z]/)) {
+            let j = lower.indexOf(str[i])
+            newStr += lower[(j+num)%26];
+        }
+        else {
+            newStr += str[i];
+        }
+    }
+    return newStr;
 };
 
 // Do not edit below this line
